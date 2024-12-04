5edb37117e60b39f135a000749bac6354d0deded
Mahedihasanimam <mdmehedihasen678@gmail.com>
2024-12-04 18:12:34 +0600

    instructor page added

--- src/app/browseCourse/instructor/[id]/page.jsx
+++ src/app/browseCourse/instructor/[id]/page.jsx
@@ -1,19 +1,19 @@
 "use client";
 import React, { useState } from "react";
-import { Rate, Tabs } from "antd";
+import { Avatar, Button, Card, Rate, Tabs } from "antd";
 import Image from "next/image";
 
-import instactor from "./Instructor.png";
+import instactor from "../../../../assets/images/Instructor2.png";
 import ratingimage from "./srahkhan.png";
 
-import { ArrowUpOutlined, PlayCircleOutlined } from "@ant-design/icons";
+import { ArrowUpOutlined, GlobalOutlined, PlayCircleOutlined } from "@ant-design/icons";
 import CourseCard from "@/components/ui/CourseCard";
 import ReviewCard from "@/components/ui/ReviewCard";
 import Link from "next/link";
 const page = ({ params }) => {
-  const [activeKey, setActiveKey] = useState("1");
+  const [activeKey, setActiveKey] = useState("2");
 
-//   course card data 
+  //   course card data 
   const coursemenu = [
     {
       id: 1,
@@ -96,8 +96,8 @@ const page = ({ params }) => {
   ];
 
 
-   // review data
-   const reviews = [
+  // review data
+  const reviews = [
     {
       name: "Sarah Khan",
       avatar: ratingimage,
@@ -131,135 +131,133 @@ const page = ({ params }) => {
         "This is a good course for someone how does not know how to code, but is interested.Each lection has projects, this is an enormous help to internalize the code. Additionally, for some projects, you need to use Google to get some specific help to get your code to work.I've learned how to code with this course, and would definitely recommend it!",
     },
   ];
-  const items = [
-    {
-      key: "1",
-      label: (
-        <label className="text-[#475467] hover:text-[#000000] cursor-pointer">
-          Courses
-        </label>
-      ),
-      children: <div className="my-12">
-      <h1 className="text-lg font-bold  text-[#475467] font-Merriweather pb-6">
-        207 courses from John Doe
-      </h1>
 
-      {/* Course cards for each category */}
-      <div className="grid  sm:grid-cols-1 grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
-        {coursemenu.map((item) => (
-          <CourseCard
-            key={item.id}
-            courseimage={item.imageLink}
-            courseTitle={item.courseTitle}
-            instructor={item.instructor}
-            rating={item.rating}
-            price={item.price}
-            reviews={item.reviews}
-            duration={item.duration}
-            students={item.students}
-            enrollLink={item.id}
-          />
-        ))}
-      </div>
-    </div>,
-    },
-    {
-      key: "2",
-      label: (
-        <label className="text-[#475467] hover:text-[#000000] cursor-pointer">
-          Reviews & ratings
-        </label>
-      ),
-      children: <div>
-        <h1 className="text-lg font-bold  text-[#475467] font-Merriweather mt-14 pb-6">
-        4.2k reviews & ratings
-      </h1>
 
-       {/* Course rating and reviews here---------------------------------------------- */}
-       <div className="  w-full ">
-              
-
-              <div className="grid lg:grid-cols-2 grid-cols-1 gap-2  ">
-                {reviews.map((review, index) => (
-                  <ReviewCard key={index} review={review} />
-                ))}
-              </div>
-              <Link
-                href={" #"}
-                className="inline-flex items-center text-[#475467] border-b-2 border-[#475467] mt-8 text-[16px] font-semibold "
-              >
-                Show all reviews
-                <ArrowUpOutlined className="rotate-45 text-xl pl-2" />
-              </Link>
-            </div>
-      </div>,
-    },
-  ];
-
- 
 
   return (
     <div>
-      <div className="max-w-[838px] mx-auto xl:px-6 p-4">
+      <div className="container mx-auto xl:px-6 p-4">
         <div className=" lg:mt-28 md:mt-24 mt-12  w-full ">
           <h1 className="text-3xl font-bold mb-24 text-[#1D2939] font-Merriweather">
             Instructor
           </h1>
-          <div className="lg:flex md:flex flex-wrap items-center justify-between">
-            <div className="flex  items-center justify-start px-2 mb-9">
-              <Image
-                height={56}
-                width={56}
-                src={instactor}
-                alt="instructor"
-                className=" rounded-full object-cover mr-4"
-              />
-              <div>
-                <h3 className="text-lg  font-semibold border-b-2 text-[#1D2939] border-[#1D2939] w-fit ">
-                  Johon Doe
-                </h3>
-                <p className="text-[#475467] text-[16px] font-normal">
-                  Head of Product Management at Google
+          <div className=" p-6 ">
+            {/* Profile Header */}
+            <div className="">
+              <Avatar size={120}>
+                <Image className="w-full" src={instactor} />
+              </Avatar>
+              <div className="mt-4">
+                <h1 className="text-xl font-semibold text-gray-800">John Doe</h1>
+                <p className="text-[#475467] flex items-center space-x-2">
+                  <span><svg width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M10.6663 13V2.33333C10.6663 1.97971 10.5259 1.64057 10.2758 1.39052C10.0258 1.14048 9.68663 1 9.33301 1H6.66634C6.31272 1 5.97358 1.14048 5.72353 1.39052C5.47348 1.64057 5.33301 1.97971 5.33301 2.33333V13M2.66634 3.66667H13.333C14.0694 3.66667 14.6663 4.26362 14.6663 5V11.6667C14.6663 12.403 14.0694 13 13.333 13H2.66634C1.92996 13 1.33301 12.403 1.33301 11.6667V5C1.33301 4.26362 1.92996 3.66667 2.66634 3.66667Z" stroke="#667085" stroke-linecap="round" stroke-linejoin="round" />
+                  </svg>
+                  </span> <span>Head of Product Management at Google</span>
                 </p>
+                <p className="text-[#475467]">Los Angeles, Southern California, U.S</p>
               </div>
             </div>
-            <div className="bg-[#F2F4F7] py-4 px-8 w-fit rounded-md">
-              <p className="text-sm text-[#101828]">Students trained</p>
-              <h1 className="text-2xl font-bold text-[#101828]">8,250</h1>
+
+            {/* Booking Section */}
+            <Card className="mt-6 shadow-sm max-w-lg">
+              <h2 className="text-lg font-bold text-[#1D2939] pb-2">Book John Doe</h2>
+              <hr />
+              <div className="flex items-center justify-between mt-4 pb-2">
+                <p className="flex items-center gap-2 text-[#344054] pb-2 text-[16px] font-semibold">
+                  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M23 7L16 12L23 17V7Z" stroke="#14698A" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M14 5H3C1.89543 5 1 5.89543 1 7V17C1 18.1046 1.89543 19 3 19H14C15.1046 19 16 18.1046 16 17V7C16 5.89543 15.1046 5 14 5Z" stroke="#14698A" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                  </svg>
+
+                  Online <span className="font-semibold">€15.00/session</span>
+                </p>
+                <hr />
+                <button type="primary" className="bg-transparent border-none">
+                  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5.83301 14.1663L14.1663 5.83301M14.1663 5.83301H5.83301M14.1663 5.83301V14.1663" stroke="#344054" stroke-width="1.67" stroke-linecap="round" stroke-linejoin="round" />
+                  </svg>
+
+                </button>
+              </div>
+              <hr />
+              <div className="flex items-center justify-between mt-4 pb-2 opacity-40">
+                <p className="flex items-center gap-2 text-[#344054] pb-2 text-[16px] font-semibold">
+                  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M20 21V19C20 17.9391 19.5786 16.9217 18.8284 16.1716C18.0783 15.4214 17.0609 15 16 15H8C6.93913 15 5.92172 15.4214 5.17157 16.1716C4.42143 16.9217 4 17.9391 4 19V21M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7Z" stroke="#E4E7EC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                  </svg>
+
+
+                  Not available
+                </p>
+                <hr />
+                <button type="primary" className="bg-transparent border-none">
+                  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5.83301 14.1663L14.1663 5.83301M14.1663 5.83301H5.83301M14.1663 5.83301V14.1663" stroke="#344054" stroke-width="1.67" stroke-linecap="round" stroke-linejoin="round" />
+                  </svg>
+
+                </button>
+              </div>
+            </Card>
+
+            {/* Availability Section */}
+            <div className="mt-6 max-w-2xl">
+              <h2 className="text-lg font-semibold text-[#1D2939]">Availability</h2>
+              <div className="grid lg:grid-cols-7 md:grid-cols-5 grid-cels-4 gap-2 mt-4 ">
+                {["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"].map((day, index) => (
+                  <div key={index} className="text-center">
+                    <p className="text-sm font-medium border rounded-sm h-[100px] w-[100px] flex  items-center justify-center ">{day}</p>
+                    {index !== 5 && index !== 6 ? (
+                      <div className="mt-2 text-blue-500 font-semibold text-sm font-medium border rounded-lg h-[100px] w-[90px] flex  items-center justify-center mr-2  "><div>
+                        3:30 <br /> <span>PM</span></div> </div>
+                    ) : (
+                      ''
+                    )}
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            {/* About Section */}
+            <div className="mt-6">
+              <h2 className="text-lg font-semibold text-gray-700">About</h2>
+              <p className="mt-4 text-gray-600">
+                I'm Angela, a developer with a passion for teaching. I'm the lead
+                instructor at the London App Brewery, London's leading Programming
+                Bootcamp. I've helped hundreds of thousands of students learn to code
+                and change their lives by becoming a developer.
+              </p>
+              <p className="mt-2 text-blue-500 cursor-pointer">Show more</p>
             </div>
           </div>
-        </div>
 
-        {/* about section -------------------------------------------------------------- */}
-        <div className="bg-white my-12   xl:max-w-2xl w-full  relative">
-          <h2 className="text-lg font-semibold text-[#475467] mb-4">About</h2>
-          <div className={`relative`}>
-            <p className="text-[#475467] mb-4">
-              I'm Angela, I'm a developer with a passion for teaching. I'm
-              the lead instructor at the London App Brewery, London's
-              leading Programming Bootcamp. I've helped hundreds of thousands of
-              students learn to code and change their lives by becoming a
-              developer. I've been invited by companies such as Twitter,
-              Facebook and Google to teach their employees
-            </p>
-            <p className="text-[#475467]">
-              My first foray into programming was when I was just 12 years old,
-              wanting to build my own Space Invader game. Since then, I've
-              made hundred of websites, apps and games. But most importantly, I
-              realised that my greatest passion is teaching.
-            </p>
+          <div className=" bg-white ">
+          <div>
+        <h1 className="text-lg font-bold  text-[#475467] font-Merriweather mt-14 pb-6">
+          4.2k reviews & ratings
+        </h1>
+
+        {/* Course rating and reviews here---------------------------------------------- */}
+        <div className="  w-full ">
+
+
+          <div className="grid lg:grid-cols-2 grid-cols-1 gap-2  ">
+            {reviews.map((review, index) => (
+              <ReviewCard key={index} review={review} />
+            ))}
           </div>
+          <Link
+            href={" #"}
+            className="inline-flex items-center text-[#475467] border-b-2 border-[#475467] mt-8 text-[16px] font-semibold "
+          >
+            Show all reviews
+            <ArrowUpOutlined className="rotate-45 text-xl pl-2" />
+          </Link>
         </div>
+      </div>,
+          </div>
 
-        <div className=" bg-white ">
-          <Tabs
-            activeKey={activeKey}
-            onChange={(key) => setActiveKey(key)}
-            items={items}
-            tabBarStyle={{ fontWeight: "bold", color: "#1890ff" }}
-          />
         </div>
-        
       </div>
     </div>
   );
